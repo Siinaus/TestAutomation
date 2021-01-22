8c53179d81eb697b3b8e016d970a2e884171a21f
Siina Kaakinen <m2936@student.jamk.fi>
2021-01-22 16:51:45 +0200

    Ensimmäiset yksikkötestit lisätty

---
+++ ArmourTester/ArmourTester.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing2018;
+
+namespace ArmourTester
+{
+    [TestClass]
+    public class ArmourTester
+    {
+        [TestMethod]
+        public void TestGetLevel()
+        {
+            // Arrange
+            // name, desc, prot, slot, level
+            // maxProt on prot (lähtötaso) ja currentProt on prot josta on vähennetty damage tai jota on korjattu
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            int lvl = ar.getLevel();
+
+            // Assert
+            if (lvl !=2)
+            {
+              Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestTakeDam()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            int slot = ar.getSlot();
+
+            // Assert
+            if (slot != 2)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestGetCondition()
+        {
+            // Arrange
+            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+
+            // Act
+            string condition = ar.getCondition();
+
+            // Assert
+
+            Assert.IsNotNull(condition);
+    
+        }
+    }
+}
