903043b3dfd5c20f7466f6a49c5709622788773d
Siina Kaakinen <m2936@student.jamk.fi>
2021-01-23 16:51:58 +0200

    Lisätty testitapauksia

--- ArmourTester/ArmourTester.cs
+++ ArmourTester/ArmourTester.cs
@@ -8,7 +8,7 @@ namespace ArmourTester
     public class ArmourTester
     {
         [TestMethod]
-        public void TestGetLevel()
+        public void TestGetLevel_Fail()
         {
             // Arrange
             // name, desc, prot, slot, level
@@ -26,7 +26,20 @@ namespace ArmourTester
         }
 
         [TestMethod]
-        public void TestTakeDam()
+        public void TestGetLevel_Ok()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            int lvl = ar.getLevel();
+
+            // Assert
+            Assert.AreEqual(2, lvl);
+        }
+
+        [TestMethod]
+        public void TestGetSlot_Fail()
         {
             // Arrange
             Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
@@ -42,10 +55,23 @@ namespace ArmourTester
         }
 
         [TestMethod]
-        public void TestGetCondition()
+        public void TestGetSlot_Ok()
         {
             // Arrange
-            Armour ar = new Armour("Jorma", "Kokkeli", 20, 2, 2);
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            int slot = ar.getSlot();
+
+            // Assert
+            Assert.AreEqual(2, slot);
+        }
+
+        [TestMethod]
+        public void TestGetCondition_Ok()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
 
             // Act
             string condition = ar.getCondition();
@@ -53,7 +79,98 @@ namespace ArmourTester
             // Assert
 
             Assert.IsNotNull(condition);
-    
+        }
+
+        [TestMethod]
+        public void TestGetCondition_Fail()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            string condition = ar.getCondition();
+
+            // Assert
+            if(condition == null)
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void TestGetCondition_RightTextMint()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            string condition = ar.getCondition();
+
+            // Assert
+
+            Assert.AreEqual("Mint", condition);
+        }
+
+        [TestMethod]
+        public void TestTakeDam_StillOk()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            ar.takeDam(5);
+            int condition = ar.getCurProt();
+
+            // Assert
+
+            Assert.AreEqual(15, condition);
+        }
+
+        [TestMethod]
+        public void TestTakeDam_Destroy()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            ar.takeDam(25);
+            string condition = ar.getCondition();
+
+            // Assert
+
+            Assert.AreEqual("Destroyed", condition);
+        }
+
+        [TestMethod]
+        public void TestRepair()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            ar.takeDam(15);
+            ar.repair(5);
+            int condition = ar.getCurProt();
+
+            // Assert
+
+            Assert.AreEqual(10, condition);
+        }
+
+        [TestMethod]
+        public void TestRepair_Fail()
+        {
+            // Arrange
+            Armour ar = new Armour("Hanska", "Kehno", 20, 2, 2);
+
+            // Act
+            ar.takeDam(15);
+            ar.repair(5);
+            int condition = ar.getCurProt();
+
+            // Assert
+            if(condition != 10)
+            {
+                Assert.Fail();
+            }
         }
     }
 }
